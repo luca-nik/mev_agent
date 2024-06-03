beec2bb5cc4c99902747e10a2e1e9fb71744dcd9
luca <luca.nicoli@sns.it>
2024-06-03 18:20:59 +0200

    Cleaning

--- exercises/first/test/maximize_surplus.py
+++ exercises/first/test/maximize_surplus.py
@@ -7,10 +7,11 @@ from matplotlib import pyplot as plt
 # Add the src directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src')))
 
-from classes import order, venue, market, agent
 import mev_project_interface as interface
 
 if __name__ == "__main__":
+    
+    # Get the json file
     parser = argparse.ArgumentParser(description='Process a JSON file path.')
     parser.add_argument('file_path', type=str, nargs='?', default=None, help='Path to the JSON file')
     args = parser.parse_args()
@@ -19,6 +20,7 @@ if __name__ == "__main__":
         print("Error: No file path provided. Please provide the path to the JSON file.")
         sys.exit(1)
     
+    # Perform MEV optimization
     interface.main(args.file_path)
 
 
